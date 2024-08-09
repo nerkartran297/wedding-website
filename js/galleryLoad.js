a5e89737f8b77096f07c53fad161ebe095387130
Khanh Tran <nerkar.tran@gmail.com>
2024-08-09 19:03:20 +0700

    added img

---
+++ js/galleryLoad.js
@@ -0,0 +1,45 @@
+const portfolio = document.querySelector('.portfolioContainer');
+
+async function galleryLoad() {
+    try {
+        const response = await fetch("gallery.json");
+        const res = await response.json();
+
+        portfolio.innerHTML = ``;
+
+        res.Gallery.forEach(gallery => {
+            const galleryContainer = document.createElement('div');
+            galleryContainer.className = 'item-portfolio item-portfolio-v2 item-4cols wedding';
+
+            galleryContainer.innerHTML = `
+            <div class="item-portfolio-holder-v2">
+                <div class="post-image item-grid-image-v2">
+                    <img
+                        loading="lazy"
+                        src="${gallery.cover}"
+                        class="img-grid-v2"
+                        alt="picture"
+                        width="750"
+                        height="750"
+                    />
+                    <a href="album_1.html"> </a>
+                </div>
+                <div class="item-text-holder-v2">
+                    <h2 class="item-title display-6">
+                        <a href="album_1.html">${gallery.name}</a>
+                    </h2>
+                    <ul class="portfolio-categ portfolio-categ-v2">
+                        <li><a href="#">${gallery.type}</a></li>
+                    </ul>
+                </div>
+            </div>
+            `;
+
+            portfolio.appendChild(galleryContainer);
+        });
+    } catch (error) {
+        console.log(`Error while fetching data ${error}`);
+    }
+}
+
+galleryLoad();
